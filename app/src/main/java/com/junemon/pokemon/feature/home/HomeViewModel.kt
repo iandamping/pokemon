67282110ccb25b31ffbbdc06b07cd5957da1656c
Ian Damping <iandamping92@gmail.com>
2026-01-08 19:27:11 +0800

    feat(ui): Define HomeViewModel to use new configuration from Repository
    - Remove usage of Backing Field with StateFlow as UiState and use DomainResult only.
    - Remove function that manipulate UiState in HomeViewModel
    - Remove usage of UiState data class in HomeScreen and use DomainResult

--- app/src/main/java/com/junemon/pokemon/feature/home/HomeViewModel.kt
+++ app/src/main/java/com/junemon/pokemon/feature/home/HomeViewModel.kt
@@ -10,14 +10,14 @@ import com.junemon.pokemon.core.data.repository.DomainResult
 import com.junemon.pokemon.core.data.repository.PokemonRepository
 import com.junemon.pokemon.core.data.repository.model.PokemonDetail
 import com.junemon.pokemon.core.di.IoDispatcher
-import com.junemon.pokemon.ui.state.ApiStates
-import com.junemon.pokemon.ui.state.UiState
 import com.junemon.pokemon.util.PokemonColorCache
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
@@ -29,10 +29,13 @@ class HomeViewModel @Inject constructor(
     @IoDispatcher private val ioDispatcher: CoroutineDispatcher
 ) : ViewModel() {
 
-    private val _pokemonDetails: MutableStateFlow<UiState<List<PokemonDetail>>> = MutableStateFlow(
-        UiState.initialize()
-    )
-    val pokemonDetails: StateFlow<UiState<List<PokemonDetail>>> get() = _pokemonDetails.asStateFlow()
+    val pokemonDetails: StateFlow<DomainResult<List<PokemonDetail>>> =
+        repository.getPokemon().stateIn(
+            scope = viewModelScope,
+            started = SharingStarted.WhileSubscribed(5000),
+            initialValue = DomainResult.Loading
+        )
+
 
     private val _pokemonColors = MutableStateFlow<Map<Int, Color>>(emptyMap())
     val pokemonColors = _pokemonColors.asStateFlow()
@@ -43,19 +46,7 @@ class HomeViewModel @Inject constructor(
 
     private fun getPokemon() {
         viewModelScope.launch {
-            _pokemonDetails.update { uiState ->
-                uiState.copy(apiState = ApiStates.LOADING)
-            }
-
-            when (val repositoryData = repository.getPokemon()) {
-                is DomainResult.Data<List<PokemonDetail>> -> _pokemonDetails.update { uiState ->
-                    uiState.copy(apiState = ApiStates.SUCCESS, data = repositoryData.data)
-                }
-
-                is DomainResult.Error -> _pokemonDetails.update { uiState ->
-                    uiState.copy(apiState = ApiStates.FAILED, errorMessage = repositoryData.message)
-                }
-            }
+            repository.refreshPokemon()
         }
     }
 
